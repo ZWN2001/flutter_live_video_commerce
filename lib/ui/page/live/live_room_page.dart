a6d534a53c8b095e17bc17bdb7ef472911ac119a
Karl Eric <2134356214@qq.com>
2024-01-10 20:35:51 +0800

    feat：弹幕聊天区，弹幕发送区

--- lib/ui/page/live/live_room_page.dart
+++ lib/ui/page/live/live_room_page.dart
@@ -3,27 +3,32 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_barrage/flutter_barrage.dart';
 import 'package:get/get.dart';
-import 'package:live_video_commerce/entity/live_room.dart';
+// import 'package:live_video_commerce/entity/live_room.dart';
+import 'package:live_video_commerce/ui/widget/live_room_chat_area.dart';
 import 'package:video_player/video_player.dart';
 
 import '../../../utils/stroke_text_widget.dart';
 
 class LiveRoomPage extends StatefulWidget {
   final int roomid;
+
   const LiveRoomPage({Key? key, required this.roomid}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _LiveRoomPageState();
 }
 
-class _LiveRoomPageState extends State<LiveRoomPage> with  TickerProviderStateMixin {
-
+class _LiveRoomPageState extends State<LiveRoomPage>
+    with TickerProviderStateMixin {
   final barrageWallController = BarrageWallController();
   Random random = Random();
   final textEditingController = TextEditingController();
   late List<Bullet> bullets;
   late VideoPlayerController _videoPlayerController;
-  TextStyle ts = const TextStyle(
+  final TextEditingController _barrageEditingController =
+      TextEditingController();
+  bool _isVideoControlAreaShowing = false;
+  TextStyle barrageTextStyle = const TextStyle(
     color: Colors.white,
     fontSize: 16,
     fontWeight: FontWeight.w500,
@@ -35,11 +40,16 @@ class _LiveRoomPageState extends State<LiveRoomPage> with  TickerProviderStateMi
     super.initState();
     bullets = List<Bullet>.generate(1000, (i) {
       final showTime = random.nextInt(60000); // in 60s
-      return Bullet(child: StrokeTextWidget('$i-$showTime',textStyle: ts,), showTime: showTime);
+      return Bullet(
+          child: StrokeTextWidget(
+            '$i-$showTime',
+            textStyle: barrageTextStyle,
+          ),
+          showTime: showTime);
     });
 
-    _videoPlayerController = VideoPlayerController.networkUrl(Uri.parse(
-        'http://202.194.15.142:7001/live/movie.flv'))
+    _videoPlayerController = VideoPlayerController.networkUrl(
+        Uri.parse('http://202.194.15.142:7001/live/movie.flv'))
       ..initialize().then((_) {
         _videoPlayerController.play();
         setState(() {});
@@ -49,56 +59,182 @@ class _LiveRoomPageState extends State<LiveRoomPage> with  TickerProviderStateMi
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("直播demo")),
-        floatingActionButton: Switch(
-          value: barrageWallController.isEnabled,
-          onChanged: (updateTo) {
-            barrageWallController.isEnabled
-                ? barrageWallController.disable()
-                : barrageWallController.enable();
-            setState(() {});
-          },
-        ),
-        body: SafeArea(
-          child: Column(children: <Widget>[
-            Expanded(
-              flex: 9,
-              child: Stack(children: <Widget>[
-                // Positioned(
-                //     top: 10,
-                //     width: Get.width,
-                //     height: Get.width * Get.size.aspectRatio + 100,
-                //     //视频播放
-                //     child: VideoPlayer(_videoPlayerController)),
-                Positioned(
-                    top: 14,
-                      width: Get.width,
-                      height: Get.width * Get.size.aspectRatio + 92,
-                  child: BarrageWall(
-                      debug: true,
-                      safeBottomHeight:
-                      60,
-                      bullets: bullets,
-                      controller: barrageWallController,
-                      child: Container()),
-                ),
-              ]),
-            ),
-            ElevatedButton(
-                onPressed: () => barrageWallController.clear(),
-                child: const Text('清空弹幕')),
-            Expanded(
-              child: TextField(
-                controller: textEditingController,
-                maxLength: 20,
-                onSubmitted: (text) {
-                  barrageWallController
-                      .send([Bullet(child: Text(text))]);
-                  textEditingController.clear();
-                },
+        appBar: AppBar(title: const Text("直播")),
+        // floatingActionButton: Switch(
+        //   value: barrageWallController.isEnabled,
+        //   onChanged: (updateTo) {
+        //     barrageWallController.isEnabled
+        //         ? barrageWallController.disable()
+        //         : barrageWallController.enable();
+        //     setState(() {});
+        //   },
+        // ),
+        body: Column(children: <Widget>[
+          SizedBox(
+            height: Get.width * Get.size.aspectRatio + 90,
+            child: videoArea(),
+          ),
+          const Expanded(child: ChatArea()),
+          // ElevatedButton(
+          //     onPressed: () => barrageWallController.clear(),
+          //     child: const Text('清空弹幕')),
+          const SizedBox(
+            height: 8,
+          ),
+          _bottomArea(),
+          const SizedBox(
+            height: 8,
+          )
+        ]));
+  }
+
+  Widget videoArea() {
+    return Stack(children: <Widget>[
+      Positioned(
+          top: 0,
+          child: Column(
+            children: [
+              Container(
+                width: Get.width,
+                height: Get.width * Get.size.aspectRatio + 90,
+                color: Colors.blue,
               ),
+              // const ChatArea()
+            ],
+          )),
+      Positioned(
+        top: 14,
+        width: Get.width,
+        height: Get.width * Get.size.aspectRatio + 92,
+        child: BarrageWall(
+            debug: false,
+            safeBottomHeight: 60,
+            bullets: bullets,
+            controller: barrageWallController,
+            child: Container()),
+      ),
+      Positioned(
+        top: 0,
+        width: Get.width,
+        height: Get.width * Get.size.aspectRatio + 90,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _isVideoControlAreaShowing = !_isVideoControlAreaShowing;
+            });
+          },
+          child: _isVideoControlAreaShowing
+              ? videoPlayerControlArea()
+              : Container(color: Colors.transparent,),
+        ),
+      )
+    ]);
+  }
+
+  Widget _bottomArea() {
+    return Row(
+      children: [
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextField(
+            controller: _barrageEditingController,
+            // cursorWidth: 1.5,
+            style: const TextStyle(
+              color: Color(0xff333333),
+              fontSize: 14.0,
             ),
-          ]),
-        ));
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.fromLTRB(12, 4, 4, 4),
+              fillColor: Colors.grey[200],
+              filled: true,
+              border: const OutlineInputBorder(borderSide: BorderSide.none),
+              hintText: '吐个槽呗~',
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(
+            Icons.send,
+            color: Colors.cyan,
+          ),
+          onPressed: () {
+            if (_barrageEditingController.text.isEmpty) return;
+            barrageWallController.send([
+              Bullet(
+                  child: StrokeTextWidget(
+                    _barrageEditingController.text,
+                    textStyle: barrageTextStyle,
+                  ),
+                  showTime: 0)
+            ]);
+            _barrageEditingController.clear();
+          },
+        )
+      ],
+    );
+  }
+
+  Widget videoPlayerControlArea(){
+    return Align(
+      alignment: Alignment.topLeft,
+      child: ShaderMask(
+          shaderCallback: (Rect bounds) {
+            return const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.8, 1],
+              colors: [
+                Color.fromRGBO(0, 0, 0, 0),
+                Color.fromRGBO(0, 0, 0, 0.8),
+              ],
+            ).createShader(bounds);
+          },
+          blendMode: BlendMode.color,
+          child: Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                    top: BorderSide(color: Color(0xffeeeeee), width: 0),
+                    bottom: BorderSide(color: Colors.white, width: 0)
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    children: [
+                      _videoPlayerController.value.isPlaying?
+                      IconButton(
+                        icon: const Icon(
+                          Icons.pause,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _videoPlayerController.pause();
+                          });
+                        },
+                      ):
+                      IconButton(
+                        icon: const Icon(
+                          Icons.play_arrow,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _videoPlayerController.play();
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              )
+          )
+      ),
+    );
   }
 }
---
+++ lib/ui/widget/live_room_chat_area.dart
@@ -0,0 +1,123 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class ChatArea extends StatefulWidget {
+  const ChatArea({super.key});
+
+  @override
+  ChatAreaState createState() => ChatAreaState();
+}
+
+class ChatAreaState extends State<ChatArea> {
+  List msgData = []; // 弹幕消息列表
+
+  final ScrollController _chatController = ScrollController(); // 弹幕区滚动Controller
+
+  @override
+  void initState() {
+    super.initState();
+    var s = {
+      'lv': 0,
+      'name': '系统消息',
+      'text': '欢迎来到直播间',
+    };
+    msgData = [s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s];
+    // SocketClient.create();
+    // var channel = SocketClient.channel;
+    // // 接受弹幕、礼物消息(webSocket)
+    // channel.stream.listen((message) {
+    //   message = json.decode(message);
+    //   var sign = message[0];
+    //   var data = message[1];
+    //   if (sign == 'getChat') {
+    //     if (mounted)
+    //       setState(() {
+    //         msgData.add(data);
+    //       });
+    //     _chatController.jumpTo(_chatController.position.maxScrollExtent);
+    //   }
+    // });
+  }
+
+  @override
+  void dispose() {
+    // SocketClient.channel?.sink?.close(status.goingAway);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+        shaderCallback: (Rect bounds) {
+          return const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 0.15],
+            colors: [
+              Colors.black, Colors.transparent
+            ],
+          ).createShader(bounds);
+        },
+        blendMode: BlendMode.dstOut,
+        child: Container(
+          decoration: const BoxDecoration(
+            border: Border(
+                top: BorderSide(color: Color(0xffeeeeee), width: 0),
+                bottom: BorderSide(color: Colors.white, width: 0)
+            ),
+          ),
+          child: ListView(
+            controller: _chatController,
+            padding: const EdgeInsets.all(10),
+            children: _chatMsg(),
+          ),
+        )
+    );
+  }
+
+  List<Widget> _chatMsg() {
+    List<Widget> msgList = [];
+    for (var item in msgData) {
+      var isAdmin = item['lv'] > 0;
+      var msgBoart = RichText(
+        strutStyle: const StrutStyle(
+            leading: 3.5 / 15,
+            height: 1
+        ),
+        text: TextSpan(
+            style: const TextStyle(
+              color: Color(0xff666666),
+              fontSize: 15,
+            ),
+            children: [
+              WidgetSpan(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: Text("LV${item['lv']}", style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 12
+                    )),
+                  )
+              ),
+              TextSpan(
+                text: '${item['name']}: ',
+                style: TextStyle(
+                    color: !isAdmin ? Colors.blue : const Color(0xff999999)
+                ),
+              ),
+              TextSpan(
+                text: item['text'],
+              ),
+            ]
+        ),
+      );
+
+      msgList.addAll([
+        msgBoart,
+        const Padding(padding: EdgeInsets.only(bottom: 5))
+      ]);
+    }
+    return msgList;
+  }
+}
