2b5eeab4dbc76494580b80d32abd6060c19ac368
Karl Eric <2134356214@qq.com>
2024-01-18 23:19:23 +0800

    feat:尝试重构直播间：speed_dial方案（弃用）购物组件可考虑该方案

--- lib/ui/page/live/live_room_page.dart
+++ lib/ui/page/live/live_room_page.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_barrage/flutter_barrage.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+// import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
@@ -29,6 +29,7 @@ class _LiveRoomPageState extends State<LiveRoomPage>
   late List<Bullet> bullets;
   late Future<void> _initializeVideoPlayerFuture;
   late StateSetter _reloadSpeedDialState;
+  bool _isVideoControlAreaShowing = false;
   late VideoPlayerController _videoPlayerController;
   final TextEditingController _barrageEditingController =
       TextEditingController();
@@ -53,11 +54,12 @@ class _LiveRoomPageState extends State<LiveRoomPage>
         floatingActionButton: StatefulBuilder(
           builder: (context, setState) {
             _reloadSpeedDialState = setState;
-            return Column(
+            return const Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                _videoPlayerControlArea(),
-                const SizedBox(
+
+                // _videoPlayerControlArea(),
+                SizedBox(
                   height: 48,
                 ),
               ],
@@ -65,7 +67,42 @@ class _LiveRoomPageState extends State<LiveRoomPage>
           },
         ),
         body: Column(children: <Widget>[
-           _videoArea(),
+           SizedBox(
+             width: Get.width,
+             child: Stack(
+               children: [
+                 _videoArea(),
+                     Positioned(
+                       top: 14,
+                       width: Get.width,
+                       height: Get.width * Get.size.aspectRatio + 40,
+                       child: BarrageWall(
+                           debug: false,
+                           safeBottomHeight: 60,
+                           bullets: bullets,
+                           controller: _barrageWallController,
+                           child: Container()),
+                     ),
+                     Positioned(
+                       top: 0,
+                       width: Get.width,
+                       height: Get.width * Get.size.aspectRatio + 50,
+                       child: GestureDetector(
+                         onTap: () {
+                           setState(() {
+                             _isVideoControlAreaShowing = !_isVideoControlAreaShowing;
+                           });
+                         },
+                         child: _isVideoControlAreaShowing
+                             ? _videoPlayerControlArea()
+                             : Container(
+                           color: Colors.transparent,
+                         ),
+                       ),
+                     )
+               ],
+             ),
+           ),
           const Expanded(
               child: Stack(
                 children: [
@@ -91,7 +128,7 @@ class _LiveRoomPageState extends State<LiveRoomPage>
           );
         } else {
           return SizedBox(
-            height: Get.width * Get.size.aspectRatio + 40,
+            height: Get.width * Get.size.aspectRatio + 50,
             child: const Center(child: CircularProgressIndicator()),
           );
         }
@@ -189,7 +226,7 @@ class _LiveRoomPageState extends State<LiveRoomPage>
     );
   }
 
-  Widget videoPlayerControlArea1() {
+  Widget _videoPlayerControlArea() {
     return Align(
       alignment: Alignment.topLeft,
       child: ShaderMask(
@@ -298,104 +335,104 @@ class _LiveRoomPageState extends State<LiveRoomPage>
               ))),
     );
   }
-
-  Widget _videoPlayerControlArea(){
-    return SpeedDial(
-      key: const ValueKey(0),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(16)),
-      ),
-      spacing: 16,
-      animationCurve: Curves.elasticInOut,
-      childMargin: EdgeInsets.zero,
-      childPadding: const EdgeInsets.all(8.0),
-      childrenButtonSize: const Size(64, 64),
-      animatedIcon: AnimatedIcons.menu_close,
-      closeManually: true,
-      children: [
-        _videoPlayerController.value.isPlaying
-            ? SpeedDialChild(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
-            child: Icon(
-              Icons.pause,
-              color: Get.theme.colorScheme.primary,
-            ),
-            // label: '暂停',
-            labelBackgroundColor: Get.theme.colorScheme.primary,
-            labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
-            onTap: () {
-              setState(() {
-                _videoPlayerController.pause();
-              });
-            })
-            : SpeedDialChild(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
-            child: Icon(
-              Icons.play_arrow,
-              color: Get.theme.colorScheme.primary,
-            ),
-            labelBackgroundColor: Get.theme.colorScheme.primary,
-            labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
-            onTap: () {
-              setState(() {
-                _videoPlayerController.play();
-              });
-            }),
-
-        _isBarrageShowing?
-
-        SpeedDialChild(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
-            child: SvgPicture.asset("assets/icon/barrage_off.svg",
-              width: 24, height: 24,
-              colorFilter: ColorFilter.mode(Get.theme.colorScheme.primary, BlendMode.srcIn),),
-            labelBackgroundColor: Get.theme.colorScheme.primary,
-            labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
-            onTap: () {
-              _bulletsStop();
-              _reloadSpeedDialState;
-            })
-            :
-        SpeedDialChild(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
-            child: SvgPicture.asset("assets/icon/barrage_on.svg",
-              width: 24, height: 24,
-              colorFilter: ColorFilter.mode(Get.theme.colorScheme.primary, BlendMode.srcIn),),
-            labelBackgroundColor: Get.theme.colorScheme.primary,
-            labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
-            onTap: () {
-              _bulletsStart();
-              _reloadSpeedDialState;
-            }),
-        SpeedDialChild(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
-            child: Icon(
-               Icons.star_border_rounded,
-              color: Get.theme.colorScheme.primary,
-            ),
-            labelBackgroundColor: Get.theme.colorScheme.primary,
-            labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
-            onTap: () async {
-              if (mounted) {
-                setState(() {
-                  // _isFavor = !_isFavor;
-                });
-              }
-
-            })
-      ],
-    );
-  }
+  //
+  // Widget _videoPlayerControlArea(){
+  //   return SpeedDial(
+  //     key: const ValueKey(0),
+  //     shape: const RoundedRectangleBorder(
+  //       borderRadius: BorderRadius.all(Radius.circular(16)),
+  //     ),
+  //     spacing: 16,
+  //     animationCurve: Curves.elasticInOut,
+  //     childMargin: EdgeInsets.zero,
+  //     childPadding: const EdgeInsets.all(8.0),
+  //     childrenButtonSize: const Size(64, 64),
+  //     animatedIcon: AnimatedIcons.menu_close,
+  //     closeManually: true,
+  //     children: [
+  //       _videoPlayerController.value.isPlaying
+  //           ? SpeedDialChild(
+  //           shape: const RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.all(Radius.circular(12)),
+  //           ),
+  //           child: Icon(
+  //             Icons.pause,
+  //             color: Get.theme.colorScheme.primary,
+  //           ),
+  //           // label: '暂停',
+  //           labelBackgroundColor: Get.theme.colorScheme.primary,
+  //           labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
+  //           onTap: () {
+  //             setState(() {
+  //               _videoPlayerController.pause();
+  //             });
+  //           })
+  //           : SpeedDialChild(
+  //           shape: const RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.all(Radius.circular(12)),
+  //           ),
+  //           child: Icon(
+  //             Icons.play_arrow,
+  //             color: Get.theme.colorScheme.primary,
+  //           ),
+  //           labelBackgroundColor: Get.theme.colorScheme.primary,
+  //           labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
+  //           onTap: () {
+  //             setState(() {
+  //               _videoPlayerController.play();
+  //             });
+  //           }),
+  //
+  //       _isBarrageShowing?
+  //
+  //       SpeedDialChild(
+  //           shape: const RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.all(Radius.circular(12)),
+  //           ),
+  //           child: SvgPicture.asset("assets/icon/barrage_off.svg",
+  //             width: 24, height: 24,
+  //             colorFilter: ColorFilter.mode(Get.theme.colorScheme.primary, BlendMode.srcIn),),
+  //           labelBackgroundColor: Get.theme.colorScheme.primary,
+  //           labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
+  //           onTap: () {
+  //             _bulletsStop();
+  //             _reloadSpeedDialState;
+  //           })
+  //           :
+  //       SpeedDialChild(
+  //           shape: const RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.all(Radius.circular(12)),
+  //           ),
+  //           child: SvgPicture.asset("assets/icon/barrage_on.svg",
+  //             width: 24, height: 24,
+  //             colorFilter: ColorFilter.mode(Get.theme.colorScheme.primary, BlendMode.srcIn),),
+  //           labelBackgroundColor: Get.theme.colorScheme.primary,
+  //           labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
+  //           onTap: () {
+  //             _bulletsStart();
+  //             _reloadSpeedDialState;
+  //           }),
+  //       SpeedDialChild(
+  //           shape: const RoundedRectangleBorder(
+  //             borderRadius: BorderRadius.all(Radius.circular(12)),
+  //           ),
+  //           child: Icon(
+  //              Icons.star_border_rounded,
+  //             color: Get.theme.colorScheme.primary,
+  //           ),
+  //           labelBackgroundColor: Get.theme.colorScheme.primary,
+  //           labelStyle: TextStyle(color: Get.theme.colorScheme.onPrimary),
+  //           onTap: () async {
+  //             if (mounted) {
+  //               setState(() {
+  //                 // _isFavor = !_isFavor;
+  //               });
+  //             }
+  //
+  //           })
+  //     ],
+  //   );
+  // }
 
 
 
